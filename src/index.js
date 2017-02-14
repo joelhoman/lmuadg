6c11cdbc91ed592970e5e87698d58ddb5a9f0a66
joelhoman <jhoman3@lion.lmu.edu>
2017-02-13 19:14:37 -0800

    refactored header/footer, started history articles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ $(function () {
     $('.navbar-placeholder').load('../navbar/navbar.html', function () {
     }); 
     $('.parallax').parallax();
+    $('.footer-placeholder').load('../footer/footer.html', function () {
+    });
 });
 
     
