445b1d53bc87bf0f901c7184c59b796333c265a9
joelhoman <jhoman3@lion.lmu.edu>
2017-04-03 03:01:37 -0700

    put mongo code in proper location

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 // const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
+const co = require('co');
 
 const app = express();
 
@@ -13,5 +15,21 @@ app.get('/', (request, response) => {
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
+});
+
+co(function* () {
+  // Connection URL
+  const url = 'mongodb://db_reader:db_reader@ds129600.mlab.com:29600/heroku_r48npwqx';
+  // Use connect method to connect to the Server
+  const db = yield MongoClient.connect(url);
+  // Close the connection
+  db.collection('eboard').find({}).toArray((err2, items) => {
+    if (err2) {
+      throw err2;
+    }
+    db.close();
+  });
+}).catch((err) => {
+  console.log(err.stack);
 });
 
