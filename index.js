6c6407cf557bc68914d0a60c3f89aadb9067c3e5
joelhoman <jhoman3@lion.lmu.edu>
2017-04-03 17:43:42 -0700

    setup server-side mongo queries and added nunjucks template

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 // const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
 const co = require('co');
+const nunjucks = require('nunjucks');
 
 const app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,22 +19,64 @@ app.get('/', (request, response) => {
   response.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.listen(app.get('port'), () => {
-  console.log('Node app is running on port', app.get('port'));
-});
+const getEboardMembers = (db, callback) => {
+  db.collection('eboard').find({}).toArray((err, items) => {
+    if (err) {
+      throw err;
+    }
+    callback(items);
+  });
+};
+
+const getHistoryArticles = (db, callback) => {
+  db.collection('history').find({}).toArray((err, items) => {
+    if (err) {
+      throw err;
+    }
+    callback(items);
+  });
+};
+
+const getNewsletterArticles = (db, callback) => {
+  db.collection('newsletter').find({}).toArray((err, items) => {
+    if (err) {
+      throw err;
+    }
+    callback(items);
+  });
+};
+
+const getHomeData = (db, callback) => {
+  db.collection('home').find({}).toArray((err, items) => {
+    if (err) {
+      throw err;
+    }
+    callback(items);
+  });
+};
 
 co(function* () {
   // Connection URL
   const url = 'mongodb://db_reader:db_reader@ds129600.mlab.com:29600/heroku_r48npwqx';
   // Use connect method to connect to the Server
   const db = yield MongoClient.connect(url);
-  // Close the connection
-  db.collection('eboard').find({}).toArray((err2, items) => {
-    if (err2) {
-      throw err2;
-    }
-    db.close();
+  app.listen(app.get('port'), () => {
+    console.log('Node app is running on port', app.get('port'));
   });
+  // Close the connection
+  getEboardMembers(db, (items) => {
+    console.log(items);
+  });
+  getHistoryArticles(db, (items) => {
+    console.log(items);
+  });
+  getNewsletterArticles(db, (items) => {
+    console.log(items);
+  });
+  getHomeData(db, (items) => {
+    console.log(items);
+  });
+  db.close();
 }).catch((err) => {
   console.log(err.stack);
 });
